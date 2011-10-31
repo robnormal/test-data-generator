f7ffca886f53eb61fbb8b7cc5c560777691e7e33
Rob Rosenbaum <rob@robrosenbaum.com>
2011-10-31 15:34:35 -0400

    Split example code into separate file, like one would expect

---
+++ example.rb
@@ -0,0 +1,27 @@
+require(File.dirname(__FILE__) + '/Table.rb')
+require(File.dirname(__FILE__) + '/Column.rb')
+
+a = TestDataGenerator::Table.new 'authors', 3, [
+  [:id],
+  [:first_name, :forgery, [:name, :first_name]],
+  [:last_name,  :forgery, [:name, :last_name]],
+  [:email,      :forgery, [:email, :address], :unique => true],
+  [:created_at],
+  [:updated_at, :datetime, :greater_than => [:authors, :created_at]]
+]
+
+b = TestDataGenerator::Table.new 'books', 3, [
+  [:id],
+  [:author_id, :belongs_to, [:authors, :id]],
+  [:title,     :words,      2..4],
+  [:isbn,      :string,     :length => 20]
+]
+
+c = TestDataGenerator::Table.new 'phone_numbers', 3, [
+  [:author_id, :belongs_to, [:authors, :id], :unique => true],
+  [:number,    :string,     :length => 10, :chars => ('0'..'9')]
+]
+
+[a,b,c].each { |t| t.each { |row| p row } }
+
+
