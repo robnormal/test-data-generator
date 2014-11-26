9ad9168607a2471f103c5f918a60e815f426ae7b
Rob Rosenbaum <rob@robrosenbaum.com>
2014-11-25 19:13:12 -0800

    ColumnData gives access via ColumnId only
    Allow Columns to read data from other columns
    whose ColumnId they know. Necessary for belongs_to

---
+++ spec/column_data_spec.rb
@@ -0,0 +1,26 @@
+require "rspec"
+require_relative "../lib/column_data"
+
+module TestDataGenerator
+  describe ColumnData do
+    context 'initialize accepts Hash{Table => Column => data}, then' do
+      before :example do
+        @cd = ColumnData.new({ tbl: { a: [1,2,3], b: [:x, :y] } })
+        @column_b = ColumnId.new(:tbl, :b)
+      end
+
+      describe :data_for do
+        it 'returns data for a column' do
+          expect(@cd.data_for(@column_b)).to contain_exactly(:x, :y)
+        end
+
+        it 'raises an ArgumentError, if no such column is found' do
+          expect { @cd.data_for(ColumnId.new(:j, :k)) }
+            .to raise_error(ArgumentError)
+        end
+      end
+    end
+  end
+end
+
+
