5887b919afd424bd193efb528246fe9959ae42c7
Rob Rosenbaum <rob@robrosenbaum.com>
2014-11-25 16:29:10 -0800

    Updated example to reflect new intended usage
    Database class will take a specification, rather
    than the Table class

--- example.rb
+++ example.rb
@@ -1,26 +1,27 @@
-require_relative('lib/Table.rb')
-require_relative('lib/Column.rb')
+require_relative('lib/database')
 
-a = TestDataGenerator::Table.new 'authors', 3, [
-  [:id],
-  [:first_name, :forgery, [:name, :first_name]],
-  [:last_name,  :forgery, [:name, :last_name]],
-  [:email,      :forgery, [:email, :address], :unique => true],
-  [:created_at],
-  [:updated_at, :datetime, [:greater_than => [:authors, :created_at]]]
-]
+a = TestDataGenerator::Database.from_spec 'publishing', {
+  authors: [3, [
+    [:id],
+    [:first_name, :forgery, [:name, :first_name]],
+    [:last_name,  :forgery, [:name, :last_name]],
+    [:email,      :forgery, [:email, :address], :unique => true],
+    [:created_at],
+    [:updated_at, :datetime, [:greater_than => [:authors, :created_at]]]
+  ]],
 
-b = TestDataGenerator::Table.new 'books', 3, [
-  [:id],
-  [:author_id, :belongs_to, [:authors, :id]],
-  [:title,     :words,      [2..4]],
-  [:isbn,      :string,     [:length => 20]]
-]
+  books: [3, [
+    [:id],
+    [:author_id, :belongs_to, [:authors, :id]],
+    [:title,     :words,      [2..4]],
+    [:isbn,      :string,     [:length => 20]]
+  ]],
 
-c = TestDataGenerator::Table.new 'phone_numbers', 3, [
-  [:author_id, :belongs_to, [:authors, :id], :unique => true],
-  [:number,    :string,     [:length => 10, :chars => ('0'..'9')]]
-]
+  phone_numbers: [3, [
+    [:author_id, :belongs_to, [:authors, :id], :unique => true],
+    [:number,    :string,     [:length => 10, :chars => ('0'..'9')]]
+  ]]
+}
 
 [a,b,c].each { |t| t.each { |row| p row } }
 
